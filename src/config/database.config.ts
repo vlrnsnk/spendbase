7d651d0e3ffabe613756d9ec5fedb6bc0c39ef40
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-04-12 09:16:16 -0400

    Add db config

---
+++ src/config/database.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_DATABASE || 'postgres',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true, // never use in production
+  logging: true,
+};

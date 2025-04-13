bc2312863eccb794e4d107c21dbf9c918f122fbc
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-04-13 19:15:49 -0400

    Add 0.0.0.0 for app to listen on

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/v1/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 
 void bootstrap();
